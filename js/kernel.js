699c799957f14809e00f9ec041c19a50e2225db4
MasakiOzeki <mai_mei_moi@proton.me>
2026-09-12 17:23:57 +0900

    feat(os): 音霊プレイヤーを足す

--- js/kernel.js
+++ js/kernel.js
@@ -1,1 +1,5 @@
-PLACEHOLDER_LOAD_FROM_FILE
+import { bits16, hash32, hexFromHash, jstDateKey, mulberry32, pick } from "./rng.js";
+import { createBus, tabId } from "./bus.js";
+import { createVfs } from "./vfs.js";
+import { attachCron } from "./cron.js";
+import { assocText, defaultAssoc, parseAssoc, resolveOpen } from "./intent.js";
