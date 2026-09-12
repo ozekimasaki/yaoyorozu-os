2cd59930ba64e8cb080666cf3464be98fe3682ca
MasakiOzeki <mai_mei_moi@proton.me>
2026-09-12 21:19:26 +0900

    test(e2e): 写しと器のシナリオを同期

--- e2e/all.mjs
+++ e2e/all.mjs
@@ -1,1 +1,4 @@
-PLACEHOLDER_LOAD_FROM_DISK
+import { mkdtempSync } from "node:fs";
+import { tmpdir } from "node:os";
+import { join } from "node:path";
+import puppeteer from "/tmp/node_modules/puppeteer-core/lib/esm/puppeteer/puppeteer-core.js";
